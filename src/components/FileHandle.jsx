9d58d7597fec06ec338917aef4ec04f9e583f5fc
iamsid47 <siddheshkulthe43@gmail.com>
2023-08-10 18:10:25 +0530

    add complete frontend for cv_ner

---
+++ src/components/FileHandle.jsx
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const FileHandle = () => {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const [jobDescription, setJobDescription] = useState("");
+  const [uploadStatus, setUploadStatus] = useState("");
+  const [rankedCvs, setRankedCvs] = useState([]);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleFileChange = (event) => {
+    setSelectedFiles(Array.from(event.target.files));
+  };
+
+  const handleJobDescriptionChange = (event) => {
+    setJobDescription(event.target.value);
+  };
+
+  const handleUpload = async () => {
+    try {
+      const formData = new FormData();
+      selectedFiles.forEach((file) => {
+        formData.append("files", file);
+      });
+
+      formData.append("jobDescription", jobDescription);
+
+      setUploadStatus("Uploading...");
+
+      const response = await axios.post(
+        "http://localhost:5000/process_cvs",
+        formData,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      );
+
+      setUploadStatus("Upload Complete");
+      setRankedCvs(response.data.ranked_cvs);
+    } catch (error) {
+      console.error("Error uploading files:", error);
+      setUploadStatus("Upload Failed");
+    }
+  };
+
+  return (
+    <section class="bg-white dark:bg-gray-900 min-h-screen flex flex-col justify-center items-center">
+      <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 ">
+        <h1 class="mb-4 text-4xl capitalize font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          Rank CVs to get the best candidate!
+        </h1>
+        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+          With the help of NER and Sentence Transformation, rank match between
+          thousands of CVs and get the best one's shortlisted 🚀
+        </p>
+        <div class="">
+          <div className="flex flex-col mb-6 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            <input
+              type="file"
+              multiple
+              onChange={handleFileChange}
+              className="text-gray-500 cursor-pointer inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center rounded-lg bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-900 transition duration-300"
+            />
+          </div>
+
+          <textarea
+            rows="10"
+            className="block p-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter job description here..."
+            value={jobDescription}
+            onChange={handleJobDescriptionChange}
+          ></textarea>
+
+          <button
+            onClick={handleUpload}
+            className="inline-flex my-6 justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 transition duration-300"
+          >
+            Upload
+          </button>
+          {uploadStatus && <p className="text-white">{uploadStatus}</p>}
+        </div>
+      </div>
+      <div class="my-8 px-4 mx-auto max-w-7xl">
+        <table class="w-full border-collapse border border-gray-300 dark:border-gray-700">
+          <thead class="bg-gray-100 dark:bg-gray-800 text-white">
+            <tr>
+              <th class="border border-gray-300 dark:border-gray-700 py-4 px-16 text-center">
+                File Name
+              </th>
+              <th class="border border-gray-300 dark:border-gray-700 py-4 px-16 text-center">
+                Rank
+              </th>
+
+              <th class="border border-gray-300 dark:border-gray-700 py-4 px-16 text-center">
+                Score
+              </th>
+              <th class="border border-gray-300 dark:border-gray-700 py-4 px-16 text-center">
+                NER Output
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {rankedCvs.map((cv, index) => (
+              <tr key={index} className="text-white">
+                <td class="border border-gray-300 dark:border-gray-700 py-2 px-4">
+                  {cv.file_name}
+                </td>
+                <td class="border border-gray-300 dark:border-gray-700 py-4 px-16 text-center">
+                  {cv.rank}
+                </td>
+
+                <td class="border border-gray-300 dark:border-gray-700 py-4 px-16 text-center">
+                  {cv.score.toFixed(2)}%
+                </td>
+                <td class="border border-gray-300 dark:border-gray-700 py-4 px-16">
+                  <button
+                    onClick={() => setShowDropdown(!showDropdown)}
+                    class="text-blue-400 py-3 underline cursor-pointer"
+                  >
+                    {showDropdown ? "Hide NER Output" : ">"}
+                  </button>
+                  {showDropdown && (
+                    <ul class="list-disc list-inside">
+                      {cv.ner_output.map((item, itemIndex) => (
+                        <li key={itemIndex}>{`${item[0]}`}</li>
+                      ))}
+                    </ul>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
+
+export default FileHandle;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav class="bg-gray-800">
+      <div class="max-w-screen-xl flex flex-wrap items-start justify-start mx-auto p-4">
+        <a href="/" class="flex items-center">
+          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            SkillMatcher
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
